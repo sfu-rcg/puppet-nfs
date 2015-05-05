90718df3d5a7ebd76089d27a6c7160bcdf780b00
Adam S <asa188@sfu.ca>
2015-05-05 05:43:56 -0700

    Adjusted to not manage services that nfs-server manages in centos7

--- manifests/client/rhel/service.pp
+++ manifests/client/rhel/service.pp
@@ -7,23 +7,13 @@ class nfs::client::rhel::service {
     require => Class['::nfs::client::rhel::configure']
   }
 
-  if $nfs::client::rhel::nfs_v4 == true {
-    $nfs4_services_ensure = 'running'
-  } else {
-    $nfs4_services_ensure = 'stopped'
-  }
-
   if $nfs::client::rhel::nfs_v4_kerberized == true {
     $nfs4_kerberized_services_ensure = 'running'
   } else {
     $nfs4_kerberized_services_ensure = 'stopped'
   }
 
-  service { ["$nfs::client::rhel::service_rpcgssd", "$nfs::client::rhel::service_rpcsvcgssd", "$nfs::client::rhel::service_rpcidmapd"]:
-    ensure    => $nfs4_kerberized_services_ensure,
-    enable    => $nfs::client::rhel::nfs_v4_kerberized,
-    hasstatus => true,
-  }
+
   if !defined(Service["$nfs::client::rhel::service_nfs"]) {
     service { "$nfs::client::rhel::service_nfs":
       ensure    => running,
@@ -32,39 +22,45 @@ class nfs::client::rhel::service {
     }    
   }
 
-  if $nfs::client::rhel::osmajor == 6 {
-    service { "$nfs::client::rhel::service_nfslock":
-      ensure    => running,
-      enable    => true,
+  if $nfs::client::rhel::osmajor <=6 {
+    service { ["$nfs::client::rhel::service_rpcgssd", "$nfs::client::rhel::service_rpcsvcgssd", "$nfs::client::rhel::service_rpcidmapd"]:
+      ensure    => $nfs4_kerberized_services_ensure,
+      enable    => $nfs::client::rhel::nfs_v4_kerberized,
       hasstatus => true,
-      require   => Service["rpcbind"], 
     }
-    service { "netfs":
-      enable  => true,
-      require => Service["$nfs::client::rhel::service_nfslock"],
+    if $nfs::client::rhel::osmajor == 6 {
+      service { "$nfs::client::rhel::service_nfslock":
+        ensure    => running,
+        enable    => true,
+        hasstatus => true,
+        require   => Service["rpcbind"], 
+      }
+      service { "netfs":
+        enable  => true,
+        require => Service["$nfs::client::rhel::service_nfslock"],
+      }
+      service { "rpcbind":
+        ensure    => running,
+        enable    => true,
+        hasstatus => true,
+        require   => [ Package["rpcbind"], Package["nfs-utils"] ],
+      }
+    } elsif $nfs::client::rhel::osmajor == 5 {
+      service { $nfs::client::rhel::service_nfslock:
+        ensure    => running,
+        enable    => true,
+        hasstatus => true,
+        require   => [ Package["portmap"], Package["nfs-utils"] ],
+      }
+      service { "netfs":
+        enable  => true,
+        require => [ Service["portmap"], Service[$nfs::client::rhel::service_nfslock] ],
+      }
+      service { "portmap":
+        ensure    => running,
+        enable    => true,
+        hasstatus => true,
+        require   => [ Package["portmap"], Package["nfs-utils"] ],
+      }
     }
-    service { "rpcbind":
-      ensure    => running,
-      enable    => true,
-      hasstatus => true,
-      require   => [ Package["rpcbind"], Package["nfs-utils"] ],
-    }
-  } elsif $nfs::client::rhel::osmajor == 5 {
-    service { $nfs::client::rhel::service_nfslock:
-      ensure    => running,
-      enable    => true,
-      hasstatus => true,
-      require   => [ Package["portmap"], Package["nfs-utils"] ],
-    }
-    service { "netfs":
-      enable  => true,
-      require => [ Service["portmap"], Service[$nfs::client::rhel::service_nfslock] ],
-    }
-    service { "portmap":
-      ensure    => running,
-      enable    => true,
-      hasstatus => true,
-      require   => [ Package["portmap"], Package["nfs-utils"] ],
-    }
-  }
 }
