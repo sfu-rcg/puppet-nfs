f2ede41af48f94def837ebfa433988a4cdaabff2
warren <warren@sfu.ca>
2019-02-01 12:48:35 -0800

    client/debian.pp: add rpcnfsdargs option

--- manifests/client/debian.pp
+++ manifests/client/debian.pp
@@ -3,6 +3,7 @@ class nfs::client::debian (
   $nfs_v4_idmap_domain = undef,
   $nfs_v4_kerberized = false,
   $nfs_v4_kerberos_realm = undef,
+  $rpcnfsdargs = undef,
   $rpcgssd_opts = undef,
   $rpcsvcgssd_opts = undef,
   $rpcidmapd_opts = undef,
